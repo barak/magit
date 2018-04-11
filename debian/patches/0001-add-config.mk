74ede5742d867a27cfada2680e29074b169bf34c
Rémi Vanicat <vanicat@debian.org>
2018-04-11 19:06:33 +0200

    Run test when autobuilding

---
+++ debian/patches/0001-add-config.mk
@@ -0,0 +1,13 @@
+Description: Add a config.mk to make testing happy
+ The test need to find where elpa package are installed
+Author: Rémi Vanicat <vanicat@debian.org>
+
+---
+Origin: debian
+Forwarded: not-needed
+Last-Update: 2018-04-11
+
+--- /dev/null
++++ magit-2.12.1/config.mk
+@@ -0,0 +1 @@
++ELPA_DIR = /usr/share/emacs/site-lisp/elpa-src
