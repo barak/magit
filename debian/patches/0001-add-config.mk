4dadf4542a45623ba8880d4a9f32f1068e0e3409
Rémi Vanicat <vanicat@debian.org>
2020-04-26 13:16:27 +0200

    Updating patches to new upstream:
    magit-repository-directories... is included upstream
    Do-not-require-async had to be updated to new version

--- debian/patches/0001-add-config.mk
+++ debian/patches/0001-add-config.mk
@@ -1,11 +1,17 @@
-Description: Add a config.mk to make testing happy
- The test need to find where elpa package are installed
-Author: Rémi Vanicat <vanicat@debian.org>
-Origin: debian
-Forwarded: not-needed
-Last-Update: 2018-04-11
+From: =?utf-8?q?R=C3=A9mi_Vanicat?= <vanicat@debian.org>
+Date: Sun, 11 Nov 2018 14:14:07 +0100
+Subject: Add a config.mk to make testing happy
 
+The test need to find where elpa package are installed
+---
+ config.mk | 1 +
+ 1 file changed, 1 insertion(+)
+ create mode 100644 config.mk
+
+diff --git a/config.mk b/config.mk
+new file mode 100644
+index 0000000..7fc9d25
 --- /dev/null
-+++ magit-2.12.1/config.mk
++++ b/config.mk
 @@ -0,0 +1 @@
 +ELPA_DIR = /usr/share/emacs/site-lisp/elpa-src
